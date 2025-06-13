e737b0f88c1dbd5912df0dc8023eda065cc3ffdb
Bohdan <bogdanvlog24@gmail.com>
2025-06-13 15:22:42 +0200

    Day 1_2 - algorythms_day1_2

---
+++ algorithms_day1_2.py
@@ -0,0 +1,28 @@
+#* numbers
+num = int(input("Enter a number:"))
+for i in range(1,11):
+    print(f"{num} x {i} = {num * i}")
+
+
+#sum numbers 1 to N
+n = int(input("enter first number:"))
+total = 0
+
+for i in range(1, n+1):
+    total += i
+print("Sum from 1 to", n, "is:", total)
+
+#checking for prime number
+nums = int(input("Enter number:"))
+is_prime = True
+if num < 2:
+    is_prime=False
+else:
+    for i in range(2, int(nums**0.5)+1):
+        if num % i == 0:
+            is_prime = False
+            break
+if is_prime:
+    print("Prime")
+else:
+    print("Not prime")
