f17f693146d73c4b61ec158a0e28046b6f3d4722
Bohdan <bogdanvlog24@gmail.com>
2025-06-14 14:00:14 +0200

    Day 2 – dict, set, tuple practice

---
+++ collections_day2.py
@@ -0,0 +1,17 @@
+person = {
+    "age":  18,
+    "name": "Bohdan",
+    "job": "Internship",
+    "city": "Prague"
+}
+del person["age"]
+
+print(person)
+
+numbers = {1,2,4,6,7,7}
+numbers.remove(7)
+print(numbers)
+
+
+my_top_language = ("Python","C++","C#")
+print(my_top_language)
