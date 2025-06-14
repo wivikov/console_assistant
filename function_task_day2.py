2cab8d0529cbce6cc29d2704cc55358ca6aa82d7
Bohdan <bogdanvlog24@gmail.com>
2025-06-14 12:46:20 +0200

    :Day 2 - Function tasks: even check and factorial

---
+++ function_task_day2.py
@@ -0,0 +1,16 @@
+def is_even(num):
+    return num % 2 == 0
+number = int(input("Enter a number:"))
+if is_even(number):
+    print("Even")
+else:
+    print("Odd")
+
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = int(input("Enter a number for factorial:"))
+print(f"Factorial of {num} is {factorial(num)}")
